8e59bfc44c8821538475901858609f59d6c6a18e
Chuối <145743624+henry-banana@users.noreply.github.com>
2025-05-20 15:43:09 +0700

    Update ex3.cpp

--- w2/ex3.cpp
+++ w2/ex3.cpp
@@ -1,27 +1,5 @@
 #include <iostream>
 
-/*
-Suppose an array of length n sorted in ascending order is rotated between 1 and n times.
-For example, the array
-nums = [0, 1, 2, 4, 5, 6, 7]
-might become:
-• [4, 5, 6, 7, 0, 1, 2] if it was rotated 4 times.
-• [0, 1, 2, 4, 5, 6, 7] if it was rotated 7 times.
-Notice that rotating an array
-[a0, a1, a2, . . . , an−1]
-one time results in the array
-[an−1, a0, a1, a2, . . . , an−2].
-Given the sorted rotated array nums of unique elements, return the minimum element
-of this array.
-Your algorithm must run in O(log n) time.
-Example
-Input
-5
-3 4 5 1 2
-Output
-1
-*/
-
 int findMin(const int arr[], int n) {
     int left = 0, right = n - 1;
     
